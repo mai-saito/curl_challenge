0fa0b5329a1bb1088376c681755e826513ef0b90
Mai Saito <mai.s@hotmail.co.jp>
2023-08-20 23:52:56 +1200

    Add DB connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import router from './routes/index';
+import mysql from 'mysql';
+import 'dotenv/config';
 
 // Initialise express
 const app = express();
@@ -17,3 +19,19 @@ const port = process.env.PORT || 3000;
 // Start the server
 app.listen(port);
 console.log('Curl Command API is listening on port ' + port);
+
+// Initialise MySQL
+const db = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_USER_PASSWORD,
+	database: process.env.DB_NAME,
+});
+
+db.connect((error) => {
+	if (error) {
+		console.error('Error connecting MySQL: ' + error);
+		return false;
+	}
+	console.log('Successfully connected to')
+})
